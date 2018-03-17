7d4805f46f464deaf247d502ab2fff20efda22f6
steinryan <hsamithdilhara@gmail.com>
2018-03-17 20:19:26 +0530

    firebase api keys, material design and create core/shared/material modules

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,5 +4,13 @@
 // The list of which env maps to which file can be found in `.angular-cli.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyAWIrPlcyDyHZUBuwl_jFoYEL656OvH9-I",
+    authDomain: "ng-fire-59918.firebaseapp.com",
+    databaseURL: "https://ng-fire-59918.firebaseio.com",
+    projectId: "ng-fire-59918",
+    storageBucket: "",
+    messagingSenderId: "565410239309"
+  }
 };
